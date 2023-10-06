78de1c7977b9f1f37f107140b858a83d23fa9424
s4lvi <salvicomputers@gmail.com>
2023-10-05 23:10:36 -0400

    added test

---
+++ server/server.py
@@ -0,0 +1,21 @@
+# /query
+# interact with the agent
+
+# /event
+# post an experience for the agent to create a memory of
+
+# memory
+# an object with metadata created from a first-person experience
+
+# embed memory
+# create embeddings from a memory
+
+# store memory
+# store memory in the vector db
+
+# recall
+# recall top n similar results from vector db, with emphasis on metadata context
+
+# 
+
+
